c868d00fd1f887860c33f3e24f44709a9458490d
ulyavrubel <uvrubel@gmail.com>
2021-10-28 20:25:05 +0300

    1 file modified

--- Arrays/twoSum.js
+++ Arrays/twoSum.js
@@ -3,6 +3,7 @@
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 // You can return the answer in any order.
+//https://leetcode.com/problems/two-sum/submissions/
 
 function twoSum(nums, target) {
   const differences = {};
