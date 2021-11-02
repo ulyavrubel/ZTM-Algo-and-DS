942802cc04a266033d9496aa1c4e4ccf3e3a8dc9
ulyavrubel <uvrubel@gmail.com>
2021-11-02 11:16:23 +0300

    maxSubArray

---
+++ Arrays/maxSubArray.js
@@ -0,0 +1,24 @@
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+// A subarray is a contiguous part of an array.
+
+function maxSubArray(nums) {
+  let sum = 0;
+  let max = nums[0];
+
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+
+    if (sum > max) {
+      max = sum;
+    }
+
+    if (sum < 0) {
+      sum = 0;
+    }
+  }
+
+  return max;
+}
+
+console.log(maxSubArray([-2, -1, -3, -4]));
